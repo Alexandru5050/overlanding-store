a45aafdbca7216a96c11a750fc4dad6d0440e885
Alexandru Vasile Sandor <alexandru.vasilesandor@gmail.com>
2024-07-11 21:34:28 +0300

    structura de baza, produse adaugate

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', dispalyAllProducts);
+const mainContainer = document.querySelector('.main');
+
+function getAllProducts() {
+    const url = "https://668d7a51099db4c579f3177f.mockapi.io/products";
+    return fetch(url).then(response => response.json());
+}
+
+function dispalyAllProducts(){
+    getAllProducts().then((products) =>
+    mainContainer.innerHTML = products.map(product => 
+        `
+        <div class="product-card flex-col gap-20 items-center justify-between">
+        <h3 class="card-title">${product.name}</h3>
+        <img src=${product.imageUrl} width="200px"/>
+        <p class="card-price">${product.price} lei</p>
+        </div>
+        
+        `
+    )
+    .join('')
+    );
+}
