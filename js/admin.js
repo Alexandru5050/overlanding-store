c92fcfc451e01f5665ddedfbc6988898acc7d1af
Alexandru Vasile Sandor <alexandru.vasilesandor@gmail.com>
2024-07-21 16:01:21 +0300

    pagina admin edit si detele

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,6 @@
+const url = "https://668d7a51099db4c579f3177f.mockapi.io/products";
+
+//load products in table at page loading
 const productsTableBody = document.getElementById('products-table').querySelector('tbody');
 console.log(productsTableBody);
 
@@ -5,10 +8,12 @@ console.log(productsTableBody);
 document.addEventListener('DOMContentLoaded', displayAllProducts);
 
 function getAllProducts() {
-    const url = "https://668d7a51099db4c579f3177f.mockapi.io/products";
     return fetch(url).then(response => response.json());
 }
 
+function getProductById(id) {
+    return fetch(`${url}/${id}`).then(response => response.json());
+}
 
 function displayAllProducts() {
     getAllProducts().then(products => {
@@ -20,12 +25,12 @@ function displayAllProducts() {
                 <img src=${product.imageUrl}
                 width="50px" />
             <td>
-                <button>
-                    <i class="fa-solid fa-pen-to-square"></i>
+                <button class="edit-${product.id}">
+                    <i class="fa-solid  fa-pen-to-square"></i>
                 </button>
             </td>
               <td>
-                <button>
+                <button class="delete-${product.id}">
                    <i class="fa-solid fa-trash"></i>
                 </button>
             </td>
@@ -34,5 +39,84 @@ function displayAllProducts() {
     )
             .join('');
     })
+}
 
+//save new product
+const form = document.getElementById('product-form');
+const nameInput = document.getElementById('name');
+const priceInput = document.getElementById('price');
+const imageUrlInput = document.getElementById('image-url');
+const detailsInput = document.getElementById('details');
+const saveProductButton = document.getElementById('save-btn');
+let editMode = false;
+let currentEditableProductId;
+saveProductButton.addEventListener('click', saveProduct);
+
+
+
+function saveProduct(event) {
+
+    event.preventDefault();
+    
+        const product = {
+            name: nameInput.value,
+            price: Number(priceInput.value),
+            imageUrl: imageUrlInput.value,
+            details: detailsInput.value,
+        };
+
+    
+
+        fetch(editMode ? `${url}/${currentEditableProductId}` : url, {
+            method: editMode? 'PUT' : 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(product),
+        }).then(() => {
+            form.reset();
+            displayAllProducts();
+            editMode = false;
+        });
+
+}
+
+//edit product
+
+productsTableBody.addEventListener('click', handleActions);
+
+function handleActions(event) {
+   
+    const className = event.target.parentElement.className;
+    if(className.includes('edit')) {
+        const productId = className.split('-')[1];
+        editProduct(productId);
+    }else if(className.includes('delete')){
+        const productId = className.split("-")[1];
+        deleteProduct(productId);
+    }
+}
+
+function editProduct(id) {
+    getProductById(id).then((product) => 
+    {
+        editMode = true;
+        nameInput.value = product.name;
+        priceInput.value = product.price;
+        imageUrlInput.value = product.imageUrl;
+        detailsInput.value = product.details;
+
+
+        currentEditableProductId = product.id;
+    }
+    );
+}
+
+function deleteProduct(id) {
+    fetch(`${url}/${id}`, {
+        method: 'DELETE',
+        
+    }).then(() => {
+        displayAllProducts();
+    });
 }
